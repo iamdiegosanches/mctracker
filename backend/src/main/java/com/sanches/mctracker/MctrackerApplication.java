8cbcc31225cf7119a0a130977a156d93b7da0917
iamdiegosanches <dgsanches3@gmail.com>
2022-07-17 16:07:42 -0300

    Project created

---
+++ backend/src/main/java/com/sanches/mctracker/MctrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sanches.mctracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MctrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MctrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sanches/mctracker/MctrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanches.mctracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MctrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
