af979e1cb5c9eb9fce352cdedc4a65b8d8155a73
iamdiegosanches <dgsanches3@gmail.com>
2022-07-23 15:56:32 -0300

    Logo edit

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import './style.css';
-import logo from '../../assets/imgs/logo.svg';
+import logo from '../../assets/imgs/logo.png';
 
 import profile from '../../assets/imgs/profile.svg'
 
@@ -14,15 +14,9 @@ function Header() {
                     <input type="search"/>
                 </div>
                 <div className="profile-area">
-                    <div className="theme-btn">
-                        <span className="material-symbols-sharp active">light_mode</span>
-                        <span className="material-symbols-sharp">dark_mode</span>
-                    </div>
                     <div className="profile">
                         <img src={profile} alt="Profile picture" />
                     </div>
-                    <h5>Profile name</h5>
-                    <span className="material-symbols-sharp">expand_more</span>
                 </div>
                 <button id="menu-btn">
                     <span className="material-symbols-sharp">menu</span>
